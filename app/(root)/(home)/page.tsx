90a3497f546e0377788922cd8546485b6991772b
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-03-01 09:35:11 +0330

    Adding auth

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,3 +1,9 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
 }
