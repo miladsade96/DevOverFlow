d57bb645fd8f70e91c7986b0eaa25e84babde31d
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-03-02 11:37:49 +0330

    Fixing main layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,47 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import "../styles/globals.css";
 import React from "react";
+import { Metadata } from "next";
+import { Inter, Space_Grotesk } from "next/font/google";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrotesk",
+});
+
+export const metadata: Metadata = {
+  title: "DevOverFlow",
+  description:
+    "The community-driven platform for asking and answering programming questions.",
+  icons: { icon: "/assets/images/site-logo.svg" },
+};
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  // @ts-ignore
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: "primary-gradient",
+          footerActionLink: "primary-text-gradient hover:text-primary-500",
+        },
+      }}
+    >
       <html lang="en">
-        <body>{children}</body>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
