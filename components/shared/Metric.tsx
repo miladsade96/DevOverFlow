45929510bfaf8adf32ad14249702d006e7899219
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-03-11 12:31:18 +0330

    Implementing Metric.tsx component

---
+++ components/shared/Metric.tsx
@@ -0,0 +1,52 @@
+import Image from "next/image";
+import Link from "next/link";
+
+interface MetricProps {
+  imgUrl: string;
+  alt: string;
+  value: string | number;
+  title: string;
+  href?: string;
+  textStyles?: string;
+  isAuthor?: boolean;
+}
+
+export default function Metric({
+  imgUrl,
+  alt,
+  value,
+  title,
+  href,
+  textStyles,
+  isAuthor,
+}: MetricProps) {
+  const metricContent = (
+    <>
+      {" "}
+      <Image
+        src={imgUrl}
+        alt={alt}
+        width={16}
+        height={16}
+        className={`object-contain ${href ? "rounded-full" : ""}`}
+      />
+      <p className={`${textStyles} flex items-center gap-1`}>
+        {value}{" "}
+        <span
+          className={`small-regular line-clamp-1 ${isAuthor ? "max-sm:hidden" : ""}`}
+        >
+          {title}
+        </span>
+      </p>
+    </>
+  );
+
+  if (href)
+    return (
+      <Link href={href} className="flex-center gap-1">
+        {metricContent}
+      </Link>
+    );
+
+  return <div className="flex-center flex-wrap gap-1">{metricContent}</div>;
+}
