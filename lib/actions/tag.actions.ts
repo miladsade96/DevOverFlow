c37c8a4f354ece7b02a45ba5a47a7e2cbc5a3eee
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-03-27 13:34:36 +0330

    Implementing getTopInteractedTags server action

---
+++ lib/actions/tag.actions.ts
@@ -0,0 +1,23 @@
+"use server";
+
+import { GetTopInteractedTagsParams } from "@/lib/actions/shared.types";
+import { connectToDatabase } from "@/lib/mongoose";
+import User from "@/database/user.model";
+
+export async function getTopInteractedTags(params: GetTopInteractedTagsParams) {
+  try {
+    await connectToDatabase();
+    const { userId } = params;
+    const user = await User.findById(userId);
+    if (!user) throw new Error("User not found!");
+    // It is not completed yet
+    return [
+      { _id: "1", name: "tag1" },
+      { _id: "2", name: "tag2" },
+      { _id: "3", name: "tag3" },
+    ];
+  } catch (e) {
+    console.log(e);
+    throw e;
+  }
+}
