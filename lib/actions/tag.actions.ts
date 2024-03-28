668493c5c2b9c23dd21e777e0cdd2c1d25b3b74e
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-03-28 13:17:10 +0330

    Implementing getAllTags server action

--- lib/actions/tag.actions.ts
+++ lib/actions/tag.actions.ts
@@ -1,8 +1,12 @@
 "use server";
 
-import { GetTopInteractedTagsParams } from "@/lib/actions/shared.types";
+import {
+  GetAllTagsParams,
+  GetTopInteractedTagsParams,
+} from "@/lib/actions/shared.types";
 import { connectToDatabase } from "@/lib/mongoose";
 import User from "@/database/user.model";
+import Tag from "@/database/tag.model";
 
 export async function getTopInteractedTags(params: GetTopInteractedTagsParams) {
   try {
@@ -16,6 +20,16 @@ export async function getTopInteractedTags(params: GetTopInteractedTagsParams) {
       { _id: "2", name: "tag2" },
       { _id: "3", name: "tag3" },
     ];
+  } catch (e) {
+    console.log(e);
+    throw e;
+  }
+}
+
+export async function getAllTags(params: GetAllTagsParams) {
+  try {
+    await connectToDatabase();
+    return await Tag.find({});
   } catch (e) {
     console.log(e);
     throw e;
