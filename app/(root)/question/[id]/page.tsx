23d16685531437b44cf5c7a902dcbaf5ed7027b4
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-03-29 14:21:32 +0330

    Implementing first version of question details page

---
+++ app/(root)/question/[id]/page.tsx
@@ -0,0 +1,62 @@
+import { getQuestionById } from "@/lib/actions/question.action";
+import Link from "next/link";
+import Image from "next/image";
+import Metric from "@/components/shared/Metric";
+import { formatAndDivideNumber, getTimestamp } from "@/lib/utils";
+import ParseHTML from "@/components/shared/ParseHTML";
+
+export default async function Page({ params }) {
+  const result = await getQuestionById({ questionId: params.id });
+  return (
+    <>
+      <div className="flex-start w-full flex-col">
+        <div className="flex w-full flex-col-reverse justify-between gap-5 sm:flex-row sm:items-center sm:gap-2">
+          <Link
+            href={`/profile/${result.author.clerkId}`}
+            className="flex items-center justify-start gap-1"
+          >
+            <Image
+              src={result.author.picture}
+              alt="Author's image"
+              className="rounded-full"
+              width={22}
+              height={22}
+            />
+            <p className="paragraph-semibold text-dark300_light700">
+              {result.author.name}
+            </p>
+          </Link>
+          <div className="flex justify-end">VOTING</div>
+        </div>
+        <h2 className="h2-semibold text-dark200_light900 mt-3.5 w-full text-left">
+          {result.title}
+        </h2>
+      </div>
+
+      <div className="mb-8 mt-5 flex flex-wrap gap-4">
+        <Metric
+          imgUrl="/assets/icons/clock.svg"
+          alt="Clock icon"
+          value={`asked ${getTimestamp(result.createdAt)}`}
+          title=" Asked"
+          textStyles="small-medium text-dark400_light800"
+        />
+        <Metric
+          imgUrl="/assets/icons/message.svg"
+          alt="Message"
+          value={result.answers.length}
+          title=" Answers"
+          textStyles="small-medium text-dark400_light800"
+        />
+        <Metric
+          imgUrl="/assets/icons/eye.svg"
+          alt="Views"
+          value={formatAndDivideNumber(result.views)}
+          title=" Views"
+          textStyles="small-medium text-dark400_light800"
+        />
+      </div>
+      <ParseHTML data={result.content} />
+    </>
+  );
+}
