97e6447f5b3e11f764c80ab10cc664dc95131e1e
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-03-31 22:51:23 +0330

    Passing necessary props to Answer component

--- app/(root)/question/[id]/page.tsx
+++ app/(root)/question/[id]/page.tsx
@@ -76,7 +76,11 @@ export default async function Page({ params }: any) {
           />
         ))}
       </div>
-      <Answer />
+      <Answer
+        question={result.content}
+        questionId={JSON.stringify(result._id)}
+        authorId={JSON.stringify(mongoUser._id)}
+      />
     </>
   );
 }
