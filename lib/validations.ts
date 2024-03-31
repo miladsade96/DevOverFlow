2e2013d829fdf5d4cd04bdf821ad7172baea2e44
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-03-31 13:38:45 +0330

    Defining AnswerSchema

--- lib/validations.ts
+++ lib/validations.ts
@@ -5,3 +5,5 @@ export const questionsSchema = z.object({
   explanation: z.string().min(100),
   tags: z.array(z.string().min(1).max(15)).min(1).max(3),
 });
+
+export const AnswerSchema = z.object({ answer: z.string().min(100) });
