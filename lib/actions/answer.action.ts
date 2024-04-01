e7da38bc83b0f37450552b10473afdb71bca3b2e
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-04-01 13:28:27 +0330

    Implementing getAnswers server action

--- lib/actions/answer.action.ts
+++ lib/actions/answer.action.ts
@@ -1,6 +1,9 @@
 "use server";
 
-import { CreateAnswerParams } from "@/lib/actions/shared.types";
+import {
+  CreateAnswerParams,
+  GetAnswersParams,
+} from "@/lib/actions/shared.types";
 import { connectToDatabase } from "@/lib/mongoose";
 import Answer from "@/database/answer.model";
 import Question from "@/database/question.model";
@@ -10,11 +13,25 @@ export async function createAnswer(params: CreateAnswerParams) {
   try {
     await connectToDatabase();
     const { content, author, question, path } = params;
-    const newAnswer = new Answer({ content, author, question });
+    const newAnswer = await Answer.create({ content, author, question });
     await Question.findByIdAndUpdate(question, {
       $push: { answers: newAnswer._id },
     });
     revalidatePath(path);
+  } catch (e) {
+    console.log(e);
+    throw e;
+  }
+}
+
+export async function getAnswers(params: GetAnswersParams) {
+  try {
+    await connectToDatabase();
+    const { questionId } = params;
+    const answers = await Answer.find({ question: questionId })
+      .populate("author", "_id clerkId name picture")
+      .sort({ createdAt: -1 });
+    return { answers };
   } catch (e) {
     console.log(e);
     throw e;
