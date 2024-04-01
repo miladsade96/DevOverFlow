e02be883612676dfbee87b32561209bc57cf16c4
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-04-01 13:29:31 +0330

    Implementing AllAnswers component

---
+++ components/shared/AllAnswers.tsx
@@ -0,0 +1,69 @@
+import Filter from "@/components/shared/Filter";
+import { AnswerFilters } from "@/constants/filters";
+import { getAnswers } from "@/lib/actions/answer.action";
+import Link from "next/link";
+import Image from "next/image";
+import { getTimestamp } from "@/lib/utils";
+import ParseHTML from "@/components/shared/ParseHTML";
+
+interface AllAnswersProps {
+  questionId: string;
+  userId: string;
+  totalAnswers: number;
+  page?: number;
+  filter?: string;
+}
+
+export default async function AllAnswers({
+  questionId,
+  userId,
+  totalAnswers,
+  page,
+  filter,
+}: AllAnswersProps) {
+  const result = await getAnswers({ questionId });
+
+  return (
+    <div className="mt-11">
+      <div className="flex items-center justify-between">
+        <h3 className="primary-text-gradient">
+          {totalAnswers} {totalAnswers > 1 ? "Answers" : "Answer"}
+        </h3>
+        <Filter filters={AnswerFilters} />
+      </div>
+      <div>
+        {result.answers.map((answer: any) => (
+          <article key={answer._id} className="light-border border-b py-10">
+            <div className="flex items-center justify-between">
+              <div className="mb-8 flex flex-col-reverse justify-between gap-5 sm:flex-row sm:items-center sm:gap-2">
+                <Link
+                  href={`/profile/${answer.author.clerkId}`}
+                  className="flex flex-1 items-start gap-1 sm:items-center"
+                >
+                  <Image
+                    className="rounded-full object-cover max-sm:mt-0.5"
+                    src={answer.author.picture}
+                    alt="Author picture"
+                    width={18}
+                    height={18}
+                  />
+                  <div className="flex flex-col sm:flex-row sm:items-center">
+                    <p className="body-semibold text-dark300_light700">
+                      {answer.author.name}
+                    </p>
+                    <p className="small-regular text-light400_light500 mt-0.5 line-clamp-1">
+                      <span className="mx-1 max-sm:hidden">-</span>
+                      answered {getTimestamp(answer.createdAt)}
+                    </p>
+                  </div>
+                </Link>
+                <div className="flex justify-end">VOTING</div>
+              </div>
+            </div>
+            <ParseHTML data={answer.content} />
+          </article>
+        ))}
+      </div>
+    </div>
+  );
+}
