b6870cf596eaefdc934682eb24b562eef1d70894
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-04-02 20:54:34 +0330

    Implementing Votes component - phase 1

--- components/shared/Votes.tsx
+++ components/shared/Votes.tsx
@@ -1,3 +1,87 @@
-export default function Votes() {
-  return <div>Votes</div>;
+"use client";
+
+import Image from "next/image";
+import { formatAndDivideNumber } from "@/lib/utils";
+
+interface VotesProps {
+  type: string;
+  itemId: string;
+  userId: string;
+  upvotes: number;
+  hasupVoted: boolean;
+  downvotes: number;
+  hasdownVoted: boolean;
+  hasSaved?: boolean;
+}
+
+export default function Votes({
+  type,
+  itemId,
+  userId,
+  upvotes,
+  hasupVoted,
+  hasdownVoted,
+  downvotes,
+  hasSaved,
+}: VotesProps) {
+  function handleSave() {}
+
+  function handleVote(action: string) {}
+
+  return (
+    <div className="flex gap-5">
+      <div className="flex-center gap-2.5">
+        <div className="flex-center gap-1.5">
+          <Image
+            src={
+              hasupVoted
+                ? "/assets/icons/upvoted.svg"
+                : "/assets/icons/upvote.svg"
+            }
+            alt="upvote"
+            height={18}
+            width={18}
+            className="cursor-pointer"
+            onClick={() => handleVote("upvote")}
+          />
+          <div className="flex-center background-light700_dark400 min-w-[18px] rounded-sm p-1">
+            <p className="subtle-medium text-dark400_light900">
+              {formatAndDivideNumber(upvotes)}
+            </p>
+          </div>
+        </div>
+        <div className="flex-center gap-1.5">
+          <Image
+            src={
+              hasdownVoted
+                ? "/assets/icons/downvoted.svg"
+                : "/assets/icons/downvote.svg"
+            }
+            alt="downvote"
+            height={18}
+            width={18}
+            className="cursor-pointer"
+            onClick={() => handleVote("downvote")}
+          />
+          <div className="flex-center background-light700_dark400 min-w-[18px] rounded-sm p-1">
+            <p className="subtle-medium text-dark400_light900">
+              {formatAndDivideNumber(downvotes)}
+            </p>
+          </div>
+        </div>
+      </div>
+      <Image
+        src={
+          hasSaved
+            ? "/assets/icons/star-filled.svg"
+            : "/assets/icons/star-red.svg"
+        }
+        alt="star"
+        height={18}
+        width={18}
+        className="cursor-pointer"
+        onClick={handleSave}
+      />
+    </div>
+  );
 }
