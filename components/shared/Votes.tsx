c60d882728738ca9ac52ae464bb1bf6016bc086e
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-04-03 13:49:19 +0330

    Implementing handleVote handler function for questions

--- components/shared/Votes.tsx
+++ components/shared/Votes.tsx
@@ -29,9 +29,53 @@ export default function Votes({
   downvotes,
   hasSaved,
 }: VotesProps) {
+  const pathname = usePathname();
+  const router = useRouter();
   function handleSave() {}
 
-  function handleVote(action: string) {}
+  async function handleVote(action: string) {
+    if (!userId) {
+      return;
+    }
+    if (action === "upvote") {
+      if (type === "Question") {
+        await upvoteQuestion({
+          questionId: JSON.parse(itemId),
+          userId: JSON.parse(userId),
+          hasupVoted,
+          hasdownVoted,
+          path: pathname,
+        });
+      } else if (type === "Answer") {
+        // await upvoteAnswer({
+        //   questionId: JSON.parse(itemId),
+        //   userId: JSON.parse(userId),
+        //   hasupVoted,
+        //   hasdownVoted,
+        //   path: pathname,
+        // });
+      }
+    }
+    if (action === "downvote") {
+      if (type === "Question") {
+        await downvoteQuestion({
+          questionId: JSON.parse(itemId),
+          userId: JSON.parse(userId),
+          hasupVoted,
+          hasdownVoted,
+          path: pathname,
+        });
+      } else if (type === "Answer") {
+        // await downvoteAnswer({
+        //   questionId: JSON.parse(itemId),
+        //   userId: JSON.parse(userId),
+        //   hasupVoted,
+        //   hasdownVoted,
+        //   path: pathname,
+        // });
+      }
+    }
+  }
 
   return (
     <div className="flex gap-5">
