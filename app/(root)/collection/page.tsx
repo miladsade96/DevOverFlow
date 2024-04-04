96de45a9be809e5400884a26f429cbb47cb1dac1
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-04-04 23:48:46 +0330

    Implementing whole collections page

---
+++ app/(root)/collection/page.tsx
@@ -0,0 +1,58 @@
+import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
+import Filter from "@/components/shared/Filter";
+import { QuestionFilters } from "@/constants/filters";
+import NoResult from "@/components/shared/NoResult";
+import QuestionCard from "@/components/cards/QuestionCard";
+import { getSavedQuestions } from "@/lib/actions/user.action";
+import { auth } from "@clerk/nextjs";
+
+export default async function Home() {
+  const { userId } = auth();
+  if (!userId) {
+    return null;
+  }
+  const result = await getSavedQuestions({ clerkId: userId });
+
+  return (
+    <>
+      <h1 className="h1-bold text-dark100_light900">Saved Questions</h1>
+      <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
+        <LocalSearchbar
+          route="/"
+          iconPosition="left"
+          imgSrc="/assets/icons/search.svg"
+          placeHolder="Search for questions"
+          otherClasses="flex-1"
+        />
+        <Filter
+          filters={QuestionFilters}
+          otherClasses="min-h-[56px] sm:min-w-[170px]"
+        />
+      </div>
+      <div className="mt-10 flex w-full flex-col gap-6">
+        {result.questions.length > 0 ? (
+          result.questions.map((question: any) => (
+            <QuestionCard
+              key={question._id}
+              _id={question._id}
+              title={question.title}
+              tags={question.tags}
+              author={question.author}
+              upvotes={question.upvotes}
+              views={question.views}
+              answers={question.answers}
+              createdAt={question.createdAt}
+            />
+          ))
+        ) : (
+          <NoResult
+            title="There is no saved question to show"
+            description="Check out the questions and save the interesting ones!"
+            link="/"
+            linkTitle="Home page"
+          />
+        )}
+      </div>
+    </>
+  );
+}
