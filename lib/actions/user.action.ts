8baf5876697fd77ad442c6856e085c11a2f04dc0
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-04-04 23:48:07 +0330

    Implementing getSavedQuestions server action function

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -6,11 +6,14 @@ import {
   CreateUserParams,
   DeleteUserParams,
   GetAllUsersParams,
+  GetSavedQuestionsParams,
   ToggleSaveQuestionParams,
   UpdateUserParams,
 } from "@/lib/actions/shared.types";
 import { revalidatePath } from "next/cache";
 import Question from "@/database/question.model";
+import { FilterQuery } from "mongoose";
+import Tag from "@/database/tag.model";
 
 export async function getUserById(params: any) {
   try {
@@ -99,6 +102,33 @@ export async function toggleSaveQuestion(params: ToggleSaveQuestionParams) {
       );
     }
     revalidatePath(path);
+  } catch (e) {
+    console.log(e);
+    throw e;
+  }
+}
+
+export async function getSavedQuestions(params: GetSavedQuestionsParams) {
+  try {
+    await connectToDatabase();
+    const { clerkId, searchQuery } = params;
+    const query: FilterQuery<typeof Question> = searchQuery
+      ? { title: { $regex: new RegExp(searchQuery, "i") } }
+      : {};
+    const user = await User.findOne({ clerkId }).populate({
+      path: "saved",
+      match: query,
+      options: { sort: { createdAt: -1 } },
+      populate: [
+        { path: "tags", model: Tag, select: "_id name" },
+        { path: "author", model: User, select: "_id clerkId name picture" },
+      ],
+    });
+    if (!user) {
+      throw new Error("User not found!");
+    }
+    const savedQuestions = user.saved;
+    return { questions: savedQuestions };
   } catch (e) {
     console.log(e);
     throw e;
