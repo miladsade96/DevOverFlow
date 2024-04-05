0cc508bb6db35e6f01c3b7e84803eba5b47e13f6
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-04-05 09:30:19 +0330

    Implementing viewQuestion server action function

--- lib/actions/interaction.action.ts
+++ lib/actions/interaction.action.ts
@@ -1,1 +1,32 @@
 "use server";
+
+import { ViewQuestionParams } from "@/lib/actions/shared.types";
+import { connectToDatabase } from "@/lib/mongoose";
+import Question from "@/database/question.model";
+import Interaction from "@/database/interaction.model";
+
+export async function viewQuestion(params: ViewQuestionParams) {
+  try {
+    await connectToDatabase();
+    const { userId, questionId } = params;
+    await Question.findByIdAndUpdate(questionId, { $inc: { views: 1 } });
+    if (userId) {
+      const existingInteraction = await Interaction.findOne({
+        user: userId,
+        question: questionId,
+        action: "view",
+      });
+      if (existingInteraction) {
+        return console.log("User has already viewed!");
+      }
+      await Interaction.create({
+        user: userId,
+        question: questionId,
+        action: "view",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
