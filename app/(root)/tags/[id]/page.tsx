450ea54974a33d9eaf3a3df81cc7976e9cd5ca56
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-04-05 12:37:53 +0330

    Implementing tag details page

--- app/(root)/tags/[id]/page.tsx
+++ app/(root)/tags/[id]/page.tsx
@@ -1,7 +1,51 @@
-export default function Page({ params }) {
+import { getQuestionsByTagId } from "@/lib/actions/tag.actions";
+import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
+import QuestionCard from "@/components/cards/QuestionCard";
+import NoResult from "@/components/shared/NoResult";
+import { URLProps } from "@/types";
+
+export default async function Page({ params, searchParams }: URLProps) {
+  const result = await getQuestionsByTagId({
+    tagId: params.id,
+    searchQuery: searchParams.q,
+  });
+  // @ts-ignore
   return (
     <>
-      <p>params: {params.id}</p>
+      <h1 className="h1-bold text-dark100_light900">{result.tagTitle}</h1>
+      <div className="mt-11 w-full">
+        <LocalSearchbar
+          route="/"
+          iconPosition="left"
+          imgSrc="/assets/icons/search.svg"
+          placeHolder="Search tag questions"
+          otherClasses="flex-1"
+        />
+      </div>
+      <div className="mt-10 flex w-full flex-col gap-6">
+        {result.questions.length > 0 ? (
+          result.questions.map((question: any) => (
+            <QuestionCard
+              key={question._id}
+              _id={question._id}
+              title={question.title}
+              tags={question.tags}
+              author={question.author}
+              upvotes={question.upvotes}
+              views={question.views}
+              answers={question.answers}
+              createdAt={question.createdAt}
+            />
+          ))
+        ) : (
+          <NoResult
+            title="There is no tagged question to show"
+            description="Check out the questions and review others!"
+            link="/"
+            linkTitle="Home page"
+          />
+        )}
+      </div>
     </>
   );
 }
