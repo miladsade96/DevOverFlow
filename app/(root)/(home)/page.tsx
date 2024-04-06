370ea0ee80f5d7423e78b7ddb0513fce74c671d8
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-04-06 13:49:25 +0330

    Removing unnecessary log statement

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -10,7 +10,6 @@ import { getQuestions } from "@/lib/actions/question.action";
 
 export default async function Home() {
   const result = await getQuestions({});
-  console.log(result);
 
   return (
     <>
