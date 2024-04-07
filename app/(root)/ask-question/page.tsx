0e8f16e51a34cb6dbb13c249924db716844688ec
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-04-07 21:45:54 +0330

    Fixing error in case user is still undefined

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -11,7 +11,7 @@ export default async function Page() {
     <div>
       <h1 className="h1-bold text-dark100_light900">Ask a Question</h1>
       <div className="mt-9">
-        <Question mongoUserId={JSON.stringify(mongoUser._id)} />
+        <Question mongoUserId={JSON.stringify(mongoUser?._id)} />
       </div>
     </div>
   );
