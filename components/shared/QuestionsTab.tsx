cb1d6cb4a1646485cbd8b056ca753361bca8ec8b
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-04-07 14:55:29 +0330

    Implementing QuestionsTab component

--- components/shared/QuestionsTab.tsx
+++ components/shared/QuestionsTab.tsx
@@ -1,3 +1,36 @@
-export default function QuestionsTab() {
-  return <p>This is the QuestionsTab component</p>;
+import { SearchParamsProps } from "@/types";
+import { getUserQuestions } from "@/lib/actions/user.action";
+import QuestionCard from "@/components/cards/QuestionCard";
+
+interface QuestionsTabProps extends SearchParamsProps {
+  userId: string;
+  clerkId?: string | undefined;
+}
+
+export default async function QuestionsTab({
+  searchParams,
+  userId,
+  clerkId,
+}: QuestionsTabProps) {
+  const result = await getUserQuestions({ userId });
+  return (
+    <>
+      {result.questions.map((question) => {
+        return (
+          <QuestionCard
+            key={question._id}
+            _id={question._id}
+            clerkId={clerkId}
+            title={question.title}
+            tags={question.tags}
+            author={question.author}
+            upvotes={question.upvotes}
+            views={question.views}
+            answers={question.answers}
+            createdAt={question.createdAt}
+          />
+        );
+      })}
+    </>
+  );
 }
