3d1779c69f9a33331d81bf9d227d8ca8cf2edac1
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-04-08 18:25:14 +0330

    Implementing editQuestion server action

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -7,6 +7,7 @@ import User from "@/database/user.model";
 import {
   CreateQuestionParams,
   DeleteQuestionParams,
+  EditQuestionParams,
   GetQuestionByIdParams,
   GetQuestionsParams,
   QuestionVoteParams,
@@ -145,5 +146,22 @@ export async function deleteQuestion(params: DeleteQuestionParams) {
   } catch (e) {
     console.log(e);
     throw e;
+  }
+}
+
+export async function editQuestion(params: EditQuestionParams) {
+  try {
+    await connectToDatabase();
+    const { questionId, title, content, path } = params;
+    const question = await Question.findById(questionId).populate("tags");
+    if (!question) {
+      throw new Error("Question not found");
+    }
+    question.title = title;
+    question.content = content;
+    await question.save();
+    revalidatePath(path);
+  } catch (error) {
+    console.log(error);
   }
 }
