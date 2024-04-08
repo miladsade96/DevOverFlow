8eb208b15f0849e075ff4e3bc9018047ab4c33d6
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-04-08 18:32:45 +0330

    Implementing question edit page

--- app/(root)/question/edit/[id]/page.tsx
+++ app/(root)/question/edit/[id]/page.tsx
@@ -1,3 +1,24 @@
-export default function Page() {
-  return <p>edit question page</p>;
+import Question from "@/components/forms/Question";
+import { auth } from "@clerk/nextjs";
+import { getUserById } from "@/lib/actions/user.action";
+import { getQuestionById } from "@/lib/actions/question.action";
+import { ParamsProps } from "@/types";
+
+export default async function Page({ params }: ParamsProps) {
+  const { userId } = auth();
+  if (!userId) return null;
+  const mongoUser = await getUserById({ userId });
+  const result = await getQuestionById({ questionId: params.id });
+  return (
+    <>
+      <h1 className="h1-bold text-dark100_light900">Edit Question</h1>
+      <div className="mt-9">
+        <Question
+          type="Edit"
+          mongoUserId={mongoUser._id}
+          questionDetails={JSON.stringify(result)}
+        />
+      </div>
+    </>
+  );
 }
