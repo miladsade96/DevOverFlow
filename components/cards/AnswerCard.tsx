c89189911f244eca3817a51027f4096a223bb996
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-04-08 18:28:13 +0330

    Resolving Link conflict with delete action button

--- components/cards/AnswerCard.tsx
+++ components/cards/AnswerCard.tsx
@@ -33,18 +33,17 @@ const AnswerCard = ({
   const showActionButtons = clerkId && clerkId === author.clerkId;
 
   return (
-    <Link
-      href={`/question/${question?._id}/#${_id}`}
-      className="card-wrapper rounded-[10px] px-11 py-9"
-    >
+    <div className="card-wrapper rounded-[10px] px-11 py-9">
       <div className="flex flex-col-reverse items-start justify-between gap-5 sm:flex-row">
         <div>
           <span className="subtle-regular text-dark400_light700 line-clamp-1 flex sm:hidden">
             {getTimestamp(createdAt)}
           </span>
-          <h3 className="sm:h3-semibold base-semibold text-dark200_light900 line-clamp-1 flex-1">
-            {question.title}
-          </h3>
+          <Link href={`/question/${question._id}`}>
+            <h3 className="sm:h3-semibold base-semibold text-dark200_light900 line-clamp-1 flex-1">
+              {question.title}
+            </h3>
+          </Link>
         </div>
         <SignedIn>
           {showActionButtons && (
@@ -74,7 +73,7 @@ const AnswerCard = ({
           />
         </div>
       </div>
-    </Link>
+    </div>
   );
 };
 
