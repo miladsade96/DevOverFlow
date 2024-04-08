dd38b884fc58a82ff4ec89f60b462ecc83c46bea
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-04-08 18:27:00 +0330

    Enabling Question form component to edit posts

--- components/forms/Question.tsx
+++ components/forms/Question.tsx
@@ -1,11 +1,9 @@
 "use client";
-
 import React, { useRef, useState } from "react";
-import { z } from "zod";
+import { Editor } from "@tinymce/tinymce-react";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
 import { useForm } from "react-hook-form";
-
-import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
@@ -16,61 +14,80 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { Button } from "../ui/button";
 import { questionsSchema } from "@/lib/validations";
-import { Editor } from "@tinymce/tinymce-react";
-import { Badge } from "@/components/ui/badge";
+import { Badge } from "../ui/badge";
 import Image from "next/image";
-import { createQuestion } from "@/lib/actions/question.action";
-import { useRouter, usePathname } from "next/navigation";
+import { createQuestion, editQuestion } from "@/lib/actions/question.action";
+import { usePathname, useRouter } from "next/navigation";
 import { useTheme } from "@/context/ThemeProvider";
 
-const type: any = "create";
-
-interface QuestionProps {
+interface Props {
+  type?: string;
   mongoUserId: string;
+  questionDetails?: string;
 }
 
-export default function Question({ mongoUserId }: QuestionProps) {
-  const editorRef = useRef(null);
+const Question = ({ type, mongoUserId, questionDetails }: Props) => {
   const { mode } = useTheme();
+  const editorRef = useRef(null);
   const [isSubmitting, setIsSubmitting] = useState(false);
-
   const router = useRouter();
-  const pathName = usePathname();
+  const pathname = usePathname();
 
+  const parsedQuestionDetails =
+    questionDetails && JSON.parse(questionDetails || "");
+
+  const groupedTags = parsedQuestionDetails?.tags.map((tag: any) => tag.name);
+
+  // 1. Define your form.
   const form = useForm<z.infer<typeof questionsSchema>>({
     resolver: zodResolver(questionsSchema),
     defaultValues: {
-      title: "",
-      explanation: "",
-      tags: [],
+      title: parsedQuestionDetails?.title || "",
+      explanation: parsedQuestionDetails?.content || "",
+      tags: groupedTags || [],
     },
   });
+
   // 2. Define a submit handler.
   async function onSubmit(values: z.infer<typeof questionsSchema>) {
     setIsSubmitting(true);
+
     try {
-      await createQuestion({
-        title: values.title,
-        content: values.explanation,
-        tags: values.tags,
-        author: JSON.parse(mongoUserId),
-        path: pathName,
-      });
-      router.push("/");
+      if (type === "Edit") {
+        await editQuestion({
+          questionId: parsedQuestionDetails._id,
+          title: values.title,
+          content: values.explanation,
+          path: pathname,
+        });
+
+        router.push(`/question/${parsedQuestionDetails._id}`);
+      } else {
+        await createQuestion({
+          title: values.title,
+          content: values.explanation,
+          tags: values.tags,
+          author: JSON.parse(mongoUserId),
+          path: pathname,
+        });
+
+        router.push("/");
+      }
     } catch (error) {
-      console.log(error);
     } finally {
       setIsSubmitting(false);
     }
   }
 
-  function handleInputKeyDown(
+  const handleInputKeyDown = (
     e: React.KeyboardEvent<HTMLInputElement>,
     field: any,
-  ) {
+  ) => {
     if (e.key === "Enter" && field.name === "tags") {
       e.preventDefault();
+
       const tagInput = e.target as HTMLInputElement;
       const tagValue = tagInput.value.trim();
 
@@ -81,6 +98,7 @@ export default function Question({ mongoUserId }: QuestionProps) {
             message: "Tag must be less than 15 characters.",
           });
         }
+
         if (!field.value.includes(tagValue as never)) {
           form.setValue("tags", [...field.value, tagValue]);
           tagInput.value = "";
@@ -90,12 +108,13 @@ export default function Question({ mongoUserId }: QuestionProps) {
         form.trigger();
       }
     }
-  }
+  };
 
-  function handleTagRemove(tag: string, field: any) {
+  const handleTagRemove = (tag: string, field: any) => {
     const newTags = field.value.filter((t: string) => t !== tag);
+
     form.setValue("tags", newTags);
-  }
+  };
 
   return (
     <Form {...form}>
@@ -113,20 +132,18 @@ export default function Question({ mongoUserId }: QuestionProps) {
               </FormLabel>
               <FormControl className="mt-3.5">
                 <Input
-                  className="no-focus paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700
-                  min-h-[56px] border"
+                  className="no-focus paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700 min-h-[56px] border"
                   {...field}
                 />
               </FormControl>
               <FormDescription className="body-regular mt-2.5 text-light-500">
-                Be specific and image you&apos;re asking a question to another
+                Be specific and imagine you&apos;re asking a question to another
                 person.
               </FormDescription>
               <FormMessage className="text-red-500" />
             </FormItem>
           )}
         />
-
         <FormField
           control={form.control}
           name="explanation"
@@ -138,14 +155,14 @@ export default function Question({ mongoUserId }: QuestionProps) {
               </FormLabel>
               <FormControl className="mt-3.5">
                 <Editor
-                  onBlur={field.onBlur}
-                  onEditorChange={(content) => field.onChange(content)}
                   apiKey={process.env.NEXT_PUBLIC_TINY_EDITOR_API_KEY}
                   onInit={(evt, editor) => {
                     // @ts-ignore
                     editorRef.current = editor;
                   }}
-                  initialValue=""
+                  onBlur={field.onBlur}
+                  onEditorChange={(content) => field.onChange(content)}
+                  initialValue={parsedQuestionDetails?.content || ""}
                   init={{
                     height: 350,
                     menubar: false,
@@ -165,13 +182,11 @@ export default function Question({ mongoUserId }: QuestionProps) {
                       "insertdatetime",
                       "media",
                       "table",
-                      "code",
-                      "wordcount",
                     ],
                     toolbar:
-                      "undo redo | blocks | codesample |" +
-                      "bold italic forecolor | alignleft aligncenter " +
-                      "alignright alignjustify | bullist numlist outdent indent",
+                      "undo redo | " +
+                      "codesample | bold italic forecolor | alignleft aligncenter |" +
+                      "alignright alignjustify | bullist numlist",
                     content_style: "body { font-family:Inter; font-size:16px }",
                     skin: mode === "dark" ? "oxide-dark" : "oxide",
                     content_css: mode === "dark" ? "dark" : "light",
@@ -186,7 +201,6 @@ export default function Question({ mongoUserId }: QuestionProps) {
             </FormItem>
           )}
         />
-
         <FormField
           control={form.control}
           name="tags"
@@ -198,28 +212,34 @@ export default function Question({ mongoUserId }: QuestionProps) {
               <FormControl className="mt-3.5">
                 <>
                   <Input
+                    disabled={type === "Edit"}
+                    className="no-focus paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700 min-h-[56px] border"
                     placeholder="Add tags..."
-                    className="no-focus paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700
-                  min-h-[56px] border"
                     onKeyDown={(e) => handleInputKeyDown(e, field)}
                   />
+
                   {field.value.length > 0 && (
                     <div className="flex-start mt-2.5 gap-2.5">
                       {field.value.map((tag: any) => (
                         <Badge
                           key={tag}
-                          onClick={() => handleTagRemove(tag, field)}
-                          className="subtle-medium background-light800_dark300 text-light400_light500 flex items-center
-                          justify-center gap-2 rounded-md border-none px-4 py-2 capitalize"
+                          className="subtle-medium background-light800_dark300 text-light400_light500 flex items-center justify-center gap-2 rounded-md border-none px-4 py-2 capitalize"
+                          onClick={() =>
+                            type !== "Edit"
+                              ? handleTagRemove(tag, field)
+                              : () => {}
+                          }
                         >
                           {tag}
-                          <Image
-                            src="/assets/icons/close.svg"
-                            alt="Close icon"
-                            width={12}
-                            height={12}
-                            className="cursor-pointer object-contain invert-0 dark:invert"
-                          />
+                          {type !== "Edit" && (
+                            <Image
+                              src="/assets/icons/close.svg"
+                              alt="Close icon"
+                              width={12}
+                              height={12}
+                              className="cursor-pointer object-contain invert-0 dark:invert"
+                            />
+                          )}
                         </Badge>
                       ))}
                     </div>
@@ -248,4 +268,6 @@ export default function Question({ mongoUserId }: QuestionProps) {
       </form>
     </Form>
   );
-}
+};
+
+export default Question;
