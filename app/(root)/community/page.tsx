78f10c4de57fb269c817e1b0ab079569eec1ff7a
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-04-11 12:49:59 +0330

    Adding search functionality to community page

--- app/(root)/community/page.tsx
+++ app/(root)/community/page.tsx
@@ -4,9 +4,10 @@ import { UserFilters } from "@/constants/filters";
 import { getAllUsers } from "@/lib/actions/user.action";
 import Link from "next/link";
 import UserCard from "@/components/cards/UserCard";
+import { SearchParamsProps } from "@/types";
 
-export default async function Page() {
-  const users = await getAllUsers({});
+export default async function Page({ searchParams }: SearchParamsProps) {
+  const users = await getAllUsers({ searchQuery: searchParams.q });
   return (
     <>
       <h1 className="h1-bold text-dark100_light900">All Users</h1>
