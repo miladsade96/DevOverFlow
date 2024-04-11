53bd7d7bd5ec7072aebae746a840f4a7c599dc3f
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-04-11 14:04:24 +0330

    Fixing the value of route prop in tags details page

--- app/(root)/tags/[id]/page.tsx
+++ app/(root)/tags/[id]/page.tsx
@@ -15,7 +15,7 @@ export default async function Page({ params, searchParams }: URLProps) {
       <h1 className="h1-bold text-dark100_light900">{result.tagTitle}</h1>
       <div className="mt-11 w-full">
         <LocalSearchbar
-          route="/"
+          route={`/tags/${params.id}`}
           iconPosition="left"
           imgSrc="/assets/icons/search.svg"
           placeHolder="Search tag questions"
