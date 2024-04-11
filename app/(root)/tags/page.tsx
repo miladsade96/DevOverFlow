916d0335d9cf95be88bd0fec767aaf03636d4357
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-04-11 13:57:57 +0330

    Adding search functionality to tags page

--- app/(root)/tags/page.tsx
+++ app/(root)/tags/page.tsx
@@ -4,9 +4,10 @@ import { TagFilters } from "@/constants/filters";
 import NoResult from "@/components/shared/NoResult";
 import { getAllTags } from "@/lib/actions/tag.actions";
 import Link from "next/link";
+import { SearchParamsProps } from "@/types";
 
-export default async function Page() {
-  const tags = await getAllTags({});
+export default async function Page({ searchParams }: SearchParamsProps) {
+  const tags = await getAllTags({ searchQuery: searchParams.q });
   return (
     <>
       <h1 className="h1-bold text-dark100_light900">All Tags</h1>
