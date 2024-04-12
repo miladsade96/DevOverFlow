832b295b4a10a591abaa4673e8daeb57841d168d
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-04-12 13:44:23 +0330

    Updating url (search params) in filter component

--- components/shared/Filter.tsx
+++ components/shared/Filter.tsx
@@ -3,11 +3,13 @@
 import {
   Select,
   SelectContent,
+  SelectGroup,
   SelectItem,
   SelectTrigger,
   SelectValue,
-  SelectGroup,
 } from "@/components/ui/select";
+import { formUrlQuery } from "@/lib/utils";
+import { useRouter, useSearchParams } from "next/navigation";
 
 interface FilterProps {
   filters: { name: string; value: string }[];
@@ -15,14 +17,26 @@ interface FilterProps {
   containerClasses?: string;
 }
 
-export default function Filter({
-  filters,
-  otherClasses,
-  containerClasses,
-}: FilterProps) {
+export default function Filter({ filters, otherClasses, containerClasses }: FilterProps) {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const paramFilter = searchParams.get("filter");
+
+  function handleUpdateParams(value: string) {
+    const newUrl = formUrlQuery({
+      params: searchParams.toString(),
+      key: "filter",
+      value,
+    });
+    router.push(newUrl, { scroll: false });
+  }
+
   return (
     <div className={`relative ${containerClasses}`}>
-      <Select>
+      <Select
+        onValueChange={(value) => handleUpdateParams(value)}
+        defaultValue={paramFilter || undefined}
+      >
         <SelectTrigger
           className={`${otherClasses} body-regular light-border background-light800_dark300 text-dark500_light700 border px-5 py-2.5`}
         >
