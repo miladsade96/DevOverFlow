a280b80c2a137963d039499a2059d3a0beee15d2
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-04-13 13:07:45 +0330

    Enabling getAllUsers server action to support page, pageSize and isNext features

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -74,7 +74,9 @@ export async function deleteUser(params: DeleteUserParams) {
 export async function getAllUsers(params: GetAllUsersParams) {
   try {
     await connectToDatabase();
-    const { searchQuery, filter } = params;
+    const { searchQuery, filter, page = 1, pageSize = 10 } = params;
+    const skipAmount = (page - 1) * pageSize;
+
     const query: FilterQuery<typeof User> = {};
     if (searchQuery) {
       query.$or = [
@@ -98,7 +100,15 @@ export async function getAllUsers(params: GetAllUsersParams) {
         break;
     }
 
-    return User.find(query).sort(sortOptions);
+    const users = await User.find(query)
+      .skip(skipAmount)
+      .limit(pageSize)
+      .sort(sortOptions);
+
+    const totalUsers = await User.countDocuments(query);
+    const isNext = totalUsers > skipAmount + users.length;
+
+    return { users, isNext };
   } catch (e) {
     console.log(e);
     throw e;
