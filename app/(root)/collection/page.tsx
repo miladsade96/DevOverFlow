1c72fdc70b50336edb3ad1607e4b2cd0c4513b0a
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-04-13 18:50:42 +0330

    Passing page search param to getSavedQuestions

--- app/(root)/collection/page.tsx
+++ app/(root)/collection/page.tsx
@@ -6,6 +6,7 @@ import QuestionCard from "@/components/cards/QuestionCard";
 import { getSavedQuestions } from "@/lib/actions/user.action";
 import { auth } from "@clerk/nextjs";
 import { SearchParamsProps } from "@/types";
+import Pagination from "@/components/shared/Pagination";
 
 export default async function Home({ searchParams }: SearchParamsProps) {
   const { userId } = auth();
@@ -16,6 +17,7 @@ export default async function Home({ searchParams }: SearchParamsProps) {
     clerkId: userId,
     searchQuery: searchParams.q,
     filter: searchParams.filter,
+    page: searchParams.page ? +searchParams.page : 1,
   });
 
   return (
