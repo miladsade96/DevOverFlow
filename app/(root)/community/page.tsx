4a44169bd9f7d463d24d448069818c6230c58d35
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-04-13 13:16:06 +0330

    Passing page search param to getAllUsers

--- app/(root)/community/page.tsx
+++ app/(root)/community/page.tsx
@@ -5,12 +5,15 @@ import { getAllUsers } from "@/lib/actions/user.action";
 import Link from "next/link";
 import UserCard from "@/components/cards/UserCard";
 import { SearchParamsProps } from "@/types";
+import Pagination from "@/components/shared/Pagination";
 
 export default async function Page({ searchParams }: SearchParamsProps) {
-  const users = await getAllUsers({
+  const result = await getAllUsers({
     searchQuery: searchParams.q,
     filter: searchParams.filter,
+    page: searchParams?.page ? +searchParams.page : 1,
   });
+
   return (
     <>
       <h1 className="h1-bold text-dark100_light900">All Users</h1>
@@ -29,8 +32,10 @@ export default async function Page({ searchParams }: SearchParamsProps) {
         />
       </div>
       <section className="mt-12 flex flex-wrap gap-4">
-        {users.length > 0 ? (
-          users.map((user) => <UserCard key={user._id} user={user} />)
+        {result.users.length > 0 ? (
+          result.users.map((user: any) => (
+            <UserCard key={user._id} user={user} />
+          ))
         ) : (
           <div className="paragraph-regular text-dark200_light800 mx-auto max-w-4xl text-center">
             <p>No users yet!</p>
@@ -40,6 +45,12 @@ export default async function Page({ searchParams }: SearchParamsProps) {
           </div>
         )}
       </section>
+      <div className="mt-10">
+        <Pagination
+          pageNumber={searchParams?.page ? +searchParams.page : 1}
+          isNext={result.isNext}
+        />
+      </div>
     </>
   );
 }
