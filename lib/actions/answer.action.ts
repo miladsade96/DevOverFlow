892a5f013ec65a411e0e70d38cbfc02a45edd7dd
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-04-14 17:44:02 +0330

    Enabling getAnswers server action to support page, pageSize and isNext features

--- lib/actions/answer.action.ts
+++ lib/actions/answer.action.ts
@@ -30,7 +30,9 @@ export async function createAnswer(params: CreateAnswerParams) {
 export async function getAnswers(params: GetAnswersParams) {
   try {
     await connectToDatabase();
-    const { questionId, sortBy } = params;
+    const { questionId, sortBy, page = 1, pageSize = 10 } = params;
+
+    const skipAmount = (page - 1) * pageSize;
 
     let sortOptions = {};
     switch (sortBy) {
@@ -52,8 +54,14 @@ export async function getAnswers(params: GetAnswersParams) {
 
     const answers = await Answer.find({ question: questionId })
       .populate("author", "_id clerkId name picture")
-      .sort(sortOptions);
-    return { answers };
+      .sort(sortOptions)
+      .skip(skipAmount)
+      .limit(pageSize);
+
+    const totalAnswers = await Answer.countDocuments({ question: questionId });
+    const isNextAnswers = totalAnswers > skipAmount + answers.length;
+
+    return { answers, isNextAnswers };
   } catch (e) {
     console.log(e);
     throw e;
