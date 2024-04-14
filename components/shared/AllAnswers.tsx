f6fb496c3908c8895a9164761e821d5672a3493f
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-04-14 17:44:30 +0330

    Adding pagination feature to all answers of a question

--- components/shared/AllAnswers.tsx
+++ components/shared/AllAnswers.tsx
@@ -7,6 +7,7 @@ import Image from "next/image";
 import { getTimestamp } from "@/lib/utils";
 import ParseHTML from "./ParseHTML";
 import Votes from "./Votes";
+import Pagination from "@/components/shared/Pagination";
 
 interface AllAnswersProps {
   questionId: string;
@@ -78,6 +79,13 @@ export default async function AllAnswers({
             <ParseHTML data={answer.content} />
           </article>
         ))}
+      </div>
+
+      <div className="mt-10 w-full">
+        <Pagination
+          pageNumber={page ? +page : 1}
+          isNext={result.isNextAnswers}
+        />
       </div>
     </div>
   );
