599bace86afdac29097dbd9a0b6d97dbb091ac84
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-04-14 16:59:50 +0330

    Enabling getUserQuestions server action to support page, pageSize and isNext

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -128,13 +128,13 @@ export async function toggleSaveQuestion(params: ToggleSaveQuestionParams) {
       await User.findByIdAndUpdate(
         userId,
         { $pull: { saved: questionId } },
-        { new: true },
+        { new: true }
       );
     } else {
       await User.findByIdAndUpdate(
         userId,
         { $addToSet: { saved: questionId } },
-        { new: true },
+        { new: true }
       );
     }
     revalidatePath(path);
@@ -217,16 +217,22 @@ export async function getUserInfo(params: GetUserByIdParams) {
 export async function getUserQuestions(params: GetUserStatsParams) {
   try {
     await connectToDatabase();
-    const { userId } = params;
+    const { userId, page = 1, pageSize = 10 } = params;
+    const skipAmount = (page - 1) * pageSize;
     const totalQuestions = await Question.countDocuments({ author: userId });
     const userQuestions = await Question.find({ author: userId })
       .sort({
         views: -1,
         upvotes: -1,
       })
+      .skip(skipAmount)
+      .limit(pageSize)
       .populate("tags", "_id name")
       .populate("author", "_id clerkId name picture");
-    return { totalQuestions, questions: userQuestions };
+
+    const isNextQuestions = totalQuestions > skipAmount + userQuestions.length;
+
+    return { totalQuestions, questions: userQuestions, isNextQuestions };
   } catch (e) {
     console.log(e);
     throw e;
