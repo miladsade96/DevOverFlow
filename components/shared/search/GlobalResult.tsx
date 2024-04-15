d3c5381032cb3bf749b6bbd0f834293bc095bb1f
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-04-15 13:26:36 +0330

    Implementing GlobalResult styles

--- components/shared/search/GlobalResult.tsx
+++ components/shared/search/GlobalResult.tsx
@@ -1,3 +1,94 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { ReloadIcon } from "@radix-ui/react-icons";
+import { useSearchParams } from "next/navigation";
+import Link from "next/link";
+import Image from "next/image";
+import GlobalFilters from "./GlobalFilters";
+
 export default function GlobalResult() {
-  return <p>global result</p>;
+  const searchParams = useSearchParams();
+  const [isLoading, setIsLoading] = useState(false);
+  const [result, setResult] = useState([
+    { type: "question", id: 1, title: "NextJS question" },
+    { type: "tag", id: 1, title: "NextJS" },
+    { type: "user", id: 1, title: "Milad Sadeghi" },
+  ]);
+  const global = searchParams.get("global");
+  const type = searchParams.get("type");
+
+  function renderLink(id: string, type: string) {
+    return "/";
+  }
+
+  useEffect(() => {
+    async function fetchResult() {
+      setIsLoading(true);
+      setResult([]);
+      try {
+        // Not yet implemented
+      } catch (err) {
+        console.log(err);
+        throw err;
+      } finally {
+        setIsLoading(false);
+      }
+    }
+  }, [global, type]);
+
+  return (
+    <div className="absolute top-full z-10 mt-3 w-full rounded-xl bg-light-800 py-5 shadow-sm dark:bg-dark-400">
+      <GlobalFilters />
+      <div className="my-5 h-px bg-light-700/50 dark:bg-dark-500/50" />
+      <div className="space-y-5">
+        <p className="text-dark400_light900 paragraph-semibold px-5">
+          Top match
+        </p>
+        {isLoading ? (
+          <div className="flex-center flex-col px-5">
+            <ReloadIcon className="my-2 size-10 animate-spin text-primary-500" />
+            <p className="text-dark200_light800 body-regular">
+              Browsing the entire database
+            </p>
+          </div>
+        ) : (
+          <div className="flex flex-col gap-2">
+            {result.length > 0 ? (
+              result.map((item: any, index: number) => (
+                <Link
+                  href={renderLink("id", "type")}
+                  key={item.id + item.type + index}
+                  className="flex w-full cursor-pointer items-start gap-3 px-5 py-2.5 hover:bg-light-700/50
+                  dark:bg-dark-500/50"
+                >
+                  <Image
+                    src="/assets/icons/tag.svg"
+                    alt="tag"
+                    width={18}
+                    height={18}
+                    className="invert-colors mt-1 object-contain"
+                  />
+                  <div className="flex flex-col">
+                    <p className="body-medium text-dark200_light800 line-clamp-1">
+                      {item.title}
+                    </p>
+                    <p className="text-light400_light500 small-medium mt-1 font-bold capitalize">
+                      {item.type}
+                    </p>
+                  </div>
+                </Link>
+              ))
+            ) : (
+              <div className="flex-center flex-col px-5">
+                <p className="text-dark200_light800 body-regular px-5 py-2.5">
+                  Oops, no results found!
+                </p>
+              </div>
+            )}
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
