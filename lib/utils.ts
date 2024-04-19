2428b016fb2e38d6b2f8805c93614f8df8eba48a
Milad Sadeghi <miladsadeghidm@gmail.com>
2024-04-19 14:19:07 +0330

    Implementing assignBadges utility function

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,8 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 import qs from "query-string";
+import { BADGE_CRITERIA } from "@/constants";
+import { BadgeCounts } from "@/types";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -94,4 +96,26 @@ export function removeKeysFromQuery({
     { url: window.location.pathname, query: currentUrl },
     { skipNull: true },
   );
+}
+
+interface assignBadgesProps {
+  criteria: { type: keyof typeof BADGE_CRITERIA; count: number }[];
+}
+
+export function assignBadges(params: assignBadgesProps) {
+  const badgeCounts: BadgeCounts = {
+    GOLD: 0,
+    SILVER: 0,
+    BRONZE: 0,
+  };
+  const { criteria } = params;
+  criteria.forEach((item) => {
+    const { type, count } = item;
+    const badgeLevels: any = BADGE_CRITERIA[type];
+    Object.keys(badgeLevels).forEach((level: any) => {
+      if (count >= badgeLevels[level])
+        badgeCounts[level as keyof BadgeCounts] += 1;
+    });
+  });
+  return badgeCounts;
 }
